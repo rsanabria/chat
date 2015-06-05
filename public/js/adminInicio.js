847fc2cbe74526c173413b66a4362e98ac4c3df6
Rodrigo Sanabria <r.sanabriadc@gmail.com>
2015-06-05 10:28:16 -0500

    archivo adminInicio cambios

--- public/js/adminInicio.js
+++ public/js/adminInicio.js
@@ -1,9 +1,6 @@
 function init() {
   var serverBaseUrl = document.domain;
 
-  /* 
-   On client init, try to connect to the socket.IO server.
-  */
   
      $("form").submit( function(event) {
                   event.preventDefault();
@@ -13,14 +10,8 @@ function init() {
  
   var socket = io.connect('http://localhost:3000');
 
-  //We'll save our session ID in a variable for later
   var sessionId = '';
 
-  /*
- When the client successfully connects to the server, an
- event "connect" is emitted. Let's get the session ID and
- log it.
-  */
   socket.on('connect', function () {
     //sacamos el identificador de sala
     sessionId = socket.io.engine.id;
